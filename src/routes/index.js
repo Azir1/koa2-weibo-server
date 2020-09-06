9e05827b451c77077874253e70bbb26d2bc3f171
Azir <1501447530@qq.com>
2020-09-06 20:47:02 +0800

    feat: ejs循环

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,22 @@ const router = require('koa-router')()
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!',
-    msg:'你好你好',
-    isAdmin:false
+    msg: '你好你好',
+    isAdmin: true,
+    blogList: [
+      {
+        id: 1,
+        title: 'aaa'
+      },
+      {
+        id: 2,
+        title: 'bbb'
+      },
+      {
+        id: 3,
+        title: 'ccc'
+      }
+    ]
   })
 })
 
